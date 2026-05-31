674d8bd8e965d3f75c219000f910239eecbec91f
lb15187042264-ship-it <lb15187042264@gmail.com>
2026-06-01 03:34:09 +0800

    Update trips.js

--- functions/trips.js
+++ functions/trips.js
@@ -1,1 +1,22 @@
+export default async function onRequest({ request, env }) {
+  const url = new URL(request.url)
+  if (request.method === 'GET') {
+    const keys = await env.TRIP_KV.list()
+    const trips = []
+    for (const k of keys.keys) {
+      const data = await env.TRIP_KV.get(k.name, 'json')
+      if (data) trips.push(data)
+    }
+    return Response.json(trips)
+  }
 
+  if (request.method === 'POST') {
+    const body = await request.json()
+    const id = Date.now().toString()
+    const trip = { id, ...body }
+    await env.TRIP_KV.put(id, JSON.stringify(trip))
+    return Response.json({ success: true })
+  }
+
+  return new Response('Method Not Allowed', { status: 405 })
+}
